573b3eb616af5ce1565480fc84759d585c05b7a4
Muhammad Ali Tanveer <tanveer7652@gmail.com>
2021-12-09 01:18:28 +0500

    Added Kruskal

--- GraphClass.py
+++ GraphClass.py
@@ -33,13 +33,13 @@ class Graph:
             line=line[1::2]
             i=0
             while i< len(line):
-                self.edges.append([node,line[i],line[i+1]])
+                self.edges.append([int(node),int(line[i]),float(line[i+1])])
                 i=i+2
 
     def generategraph(self):
 
         for i in range(self.nodecount):
-            self.graph.add_node(self.nodeandpos[i][0], pos=(float(self.nodeandpos[i][1]), float(self.nodeandpos[i][2])))
+            self.graph.add_node(int(self.nodeandpos[i][0]), pos=(float(self.nodeandpos[i][1]), float(self.nodeandpos[i][2])))
 
         for i in range(len(self.edges)):
             self.graph.add_edge(self.edges[i][0],self.edges[i][1], weight=float(self.edges[i][2])/10000000)
@@ -51,6 +51,44 @@ class Graph:
             labels = nx.get_edge_attributes(self.graph,'weight')
             nx.draw_networkx_edge_labels(self.graph,pos,edge_labels=labels)
         plt.savefig('graph.png', bbox_inches='tight')
+
+    def find(self, parent, i):
+        if parent[i] == i:
+            return i
+        return self.find(parent, parent[i])
+
+    def apply_union(self, parent, rank, x, y):
+        xroot = self.find(parent, x)
+        yroot = self.find(parent, y)
+        if rank[xroot] < rank[yroot]:
+            parent[xroot] = yroot
+        elif rank[xroot] > rank[yroot]:
+            parent[yroot] = xroot
+        else:
+            parent[yroot] = xroot
+            rank[xroot] += 1
+
+    def kruskal_algo(self):
+        result = []
+        i, e = 0, 0
+        MST = sorted(self.edges, key=lambda item: item[2])
+        parent = []
+        rank = []
+        for node in range(self.nodecount):
+            parent.append(node)
+            rank.append(0)
+        while e < self.nodecount - 1:
+            u, v, w = self.edges[i]
+            i = i + 1
+            x = self.find(parent, u)
+            y = self.find(parent, v)
+            if x != y:
+                e = e + 1
+                result.append([u, v, w])
+                self.apply_union(parent, rank, x, y)
+        return result
+    
+
 
     
         
--- project.py
+++ project.py
@@ -13,42 +13,7 @@ ui.setupUi(MainWindow)
 MainWindow.show()
 ui.FileButton.clicked.connect(lambda: ui.getfile())
 ui.GraphItbutton.clicked.connect(lambda: ui.graphit(ui.InputBox.text(),obj))
+ui.runalgobutton.clicked.connect(lambda: ui.test(obj))
 sys.exit(app.exec_())
 
 
-""" if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    ui.tabWidget.setCurrentIndex(0)
-
-    ui.Chapter2Button.clicked.connect(lambda: ui.MovetoChapter2(MainWindow))
-    ui.Chapter3Button.clicked.connect(lambda: ui.MovetoChapter3(MainWindow))
-    ui.Chapter5Button.clicked.connect(lambda: ui.MovetoChapter5(MainWindow))
-    ui.Chapter4Button.clicked.connect(lambda: ui.MovetoChapter4(MainWindow))
-
-    ui.Chp2startbutton.clicked.connect(lambda: ui.Chapter2Start(MainWindow))
-    ui.Chp2mainback.clicked.connect(lambda: ui.MovetoMain(MainWindow))
-    ui.Chp2iterback.clicked.connect(lambda: ui.MovetoChapter2(MainWindow))
-    ui.Chp2choicebox.currentIndexChanged.connect(lambda: ui.Chp2ChoiceChanged(MainWindow))
-    
-    ui.StartInterpolationButton.clicked.connect(lambda: ui.Chapter3Start(MainWindow))
-    ui.Chp3pointsbox.currentIndexChanged.connect(lambda: ui.Chp3pointschanged(MainWindow))
-    ui.Chp3mainback.clicked.connect(lambda: ui.MovetoMain(MainWindow))
-    ui.Chp3iterback.clicked.connect(lambda: ui.MovetoChapter3(MainWindow))
-    ui.Ch3choicebox.currentIndexChanged.connect(lambda: ui.Chp3ChoiceChanged(MainWindow))
-
-    ui.Ch4choicebox.currentIndexChanged.connect(lambda: ui.Chapter4MethodChanged(MainWindow))
-    ui.Chp4pointsbox.currentIndexChanged.connect(lambda: ui.Chapter4pointschanged(MainWindow))
-    ui.Ch4StartButton.clicked.connect(lambda: ui.Chapter4Start(MainWindow))
-    ui.Chp4mainback.clicked.connect(lambda: ui.MovetoMain(MainWindow))
-    ui.Ch4iterback.clicked.connect(lambda: ui.MovetoChapter4(MainWindow))
-
-    ui.Ch5StartButton.clicked.connect(lambda: ui.Chapter5Start(MainWindow))
-    ui.Chp5mainback.clicked.connect(lambda: ui.MovetoMain(MainWindow))
-    ui.Ch5AnsBackButton.clicked.connect(lambda:ui.MovetoChapter5(MainWindow))
-
-    sys.exit(app.exec_()) """
